837a065ff75b977d7082ae4b2942cba5c3ba5082
rokari-19 <oshomahiyelobu1@gmail.com>
2024-04-29 22:44:06 +0100

    changed currency on frontend from dollar to naira

--- src/components/OrderSummary.vue
+++ src/components/OrderSummary.vue
@@ -20,9 +20,9 @@
                     v-bind:key="item.product.id"
                 >
                     <td>{{ item.product.name }}</td>
-                    <td>${{ item.product.price }}</td>
+                    <td>NGN {{ item.product.price }}</td>
                     <td>{{ item.quantity }}</td>
-                    <td>${{ getItemTotal(item).toFixed(2) }}</td>
+                    <td>NGN {{ getItemTotal(item).toFixed(2) }}</td>
                 </tr>
             </tbody>
         </table>
